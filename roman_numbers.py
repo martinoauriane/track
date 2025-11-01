1325c8af8ecb08e8dba8474d2162a9a7189b2126
movauriane <auriane.martino@movu-robotics.com>
2025-11-01 22:26:17 +0100

    roman_numbers converter

---
+++ roman_numbers.py
@@ -0,0 +1,32 @@
+""" ROMAN NUMBERS """
+
+numbers_dict = {
+  "I": 1, 
+  "V": 5,
+  "X": 10, 
+  "L": 50, 
+  "C": 100
+}
+
+def convert(roman_numbers: str):
+  last = ""
+  final_number = 0
+  for number in roman_numbers:
+    if last == "":
+      final_number += numbers_dict[number]
+      last = number
+    else :
+      number_before = numbers_dict[last]
+      current_number = numbers_dict[number]
+      if number_before < current_number:
+        final_number += current_number - number_before * 2
+        last = number
+      else :
+        final_number += current_number
+        last = number
+  print("number is", final_number)
+  return final_number
+
+convert("XII")
+
+
